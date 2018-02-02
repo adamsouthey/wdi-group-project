21a88305e16dcf302323cfff3326bc05eb3f08ac
Amadea Kimmins <amadea.kimmins@hotmail.co.uk>
2018-02-02 14:15:27 +0000

    registration successful

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router  = express.Router();
+const auth = require('../controllers/auth');
 
-// Routes go here
+router.route('/register')
+  .post(auth.register);
+
+router.route('/login')
+  .post(auth.login);
 
 module.exports = router;

516d18b4f6bab91373890cedc62fb3baf307aa07
Amadea Kimmins <amadea.kimmins@hotmail.co.uk>
2018-02-02 14:29:30 +0000

    user model, bcrypt and JWT authentication

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const router  = express.Router();
 const auth = require('../controllers/auth');
+// const secureRoute = require('../lib/secureRoute');
+// const groups = require('../controllers/groups');
+//
+// router.route('/groups')
+//   .all(secureRoute)
+//   .get(groups.index)
+//   .post(groups.create);
+//
+// router.route('/groups/:id')
+//   .all(secureRoute)
+//   .get(groups.show)
+//   .put(groups.update)
+//   .delete(groups.delete);
 
 router.route('/register')
   .post(auth.register);

6b20a3dc91243792892179bfd48854e4fb83b7d2
adamsouthey <adamjsouthey@gmail.com>
2018-02-03 13:44:19 +0000

    working copy

--- src/js/controllers/groups/index.controller.js
+++ src/js/controllers/groups/index.controller.js
@@ -1,9 +1,26 @@
+// angular
+//   .module('eventApp')
+//   .controller('GroupsIndexCtrl', GroupsIndexCtrl);
+//
+// GroupsIndexCtrl.$inject = [ 'Group'];
+// function GroupsIndexCtrl(Group) {
+//   const vm = this;
+//   vm.all = Group.query();
+// }
 angular
   .module('eventApp')
   .controller('GroupsIndexCtrl', GroupsIndexCtrl);
 
-GroupsIndexCtrl.$inject = [ 'Group'];
-function GroupsIndexCtrl(Group) {
+
+GroupsIndexCtrl.$inject = ['$http'];
+function GroupsIndexCtrl($http) {
   const vm = this;
-  vm.all = Group.query();
+
+
+  $http
+    .get('https://api.meetup.com/find/upcoming_events?photo-host=public&page=20&sig_id=240914666&sig=d7ab433738473e8efe8f309096e2d34dd67fd047')
+    .then((res) => {
+      vm.all = res.data;
+      console.log('hello');
+    });
 }

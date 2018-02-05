c1426c4eb70ab057ba4a59505736a7ce75d63ea6
adamsouthey <adamjsouthey@gmail.com>
2018-02-05 16:37:17 +0000

    added google maps to view show page and updated information on show page

---
+++ src/js/directives/googleMap.directive.js
@@ -0,0 +1,34 @@
+angular
+  .module('eventApp')
+  .directive('googleMap', googleMap);
+
+googleMap.$inject = ['$window'];
+function googleMap($window) {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: '<div class="google-map"></div>',
+    scope: {
+      center: '='
+    },
+    link(scope, element) {
+
+      scope.$watch('center', () => {
+        if (!scope.center.lat || !scope.center.lng) return false;
+
+        const map = new $window.google.maps.Map(element[0], {
+          zoom: 8,
+          center: scope.center
+
+        });
+
+        new $window.google.maps.Marker({
+          position: scope.center,
+          map: map
+        });
+      });
+
+
+    }
+  };
+}

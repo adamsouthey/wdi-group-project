bc5d0d24bbf4285ac5e1c862d57be432757c8d51
adamsouthey <adamjsouthey@gmail.com>
2018-02-07 16:41:13 +0000

    added user info to registration + profile show page

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
   lastname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  country: { type: String, required: true },
   image: { type: String }
 });
 

cf088c9309a1b375f2a61ee6a6305ba5cf5552bb
Amadea Kimmins <amadea.kimmins@hotmail.co.uk>
2018-02-08 15:45:48 +0000

    user tests passing

---
+++ test/api/controllers/groups.js
@@ -0,0 +1,6 @@
+/* globals api, expect, it, describe, afterEach, beforeEach */
+require('../../spec_helper');
+
+const Group = require('../../../models/group');
+
+describe('Groups Controller Tests')

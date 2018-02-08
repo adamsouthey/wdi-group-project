18ba2860b93a356d4a98207d91d3bf959b183d65
adamsouthey <adamjsouthey@gmail.com>
2018-02-08 09:44:07 +0000

    added a snazzy map

--- src/js/directives/googleMap.directive.js
+++ src/js/directives/googleMap.directive.js
@@ -18,7 +18,8 @@ function googleMap($window) {
 
         const map = new $window.google.maps.Map(element[0], {
           zoom: 14,
-          center: scope.center
+          center: scope.center,
+          styles: [{'stylers': [{'hue': '#dd0d0d'}]},{'featureType': 'road','elementType': 'labels','stylers': [{'visibility':'off'}]},{'featureType': 'road','elementType': 'geometry','stylers': [{'lightness':100},{'visibility': 'simplified'}]}]
 
         });
 
